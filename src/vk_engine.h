187d7b56b9f0001b8ba15df8b4ae140ff5b2d21a
이해성 <haesoong@ihaeseongs-iMac.local>
2025-11-24 16:18:25 +0900

    Chapter 4: Textures and Engine Architecture[~ Meshes and Camera]

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -65,6 +65,36 @@ struct GPUSceneData {
     glm::vec4 sunlightColor;
 };
 
+struct GLTFMetallic_Roughness {
+    MaterialPipeline opaquePipeline;
+    MaterialPipeline transparentPipeline;
+
+    VkDescriptorSetLayout materialLayout;
+
+    struct MaterialConstants {
+        glm::vec4 colorFactors;
+        glm::vec4 metal_rough_factors;
+        //padding, we need it anyway for uniform buffers
+        glm::vec4 extra[14];
+    };
+
+    struct MaterialResources {
+        AllocatedImage colorImage;
+        VkSampler colorSampler;
+        AllocatedImage metalRoughImage;
+        VkSampler metalRoughSampler;
+        VkBuffer dataBuffer;
+        uint32_t dataBufferOffset;
+    };
+
+    DescriptorWriter writer;
+
+    void build_pipelines(VulkanEngine* engine);
+    void clear_resources(VkDevice device);
+
+    MaterialInstance write_material(VkDevice device, MaterialPass pass, const MaterialResources& resources, DescriptorAllocatorGrowable& descriptorAllocator);
+};
+
 class VulkanEngine
 {
 public:
@@ -86,7 +116,7 @@ public:
 
     DeletionQueue _mainDeletionQueue;
 
-    DescriptorAllocator globalDescriptorAllocator;
+    DescriptorAllocatorGrowable globalDescriptorAllocator;
 
     VkDescriptorSet _drawImageDescriptors;
     VkDescriptorSetLayout _drawImageDescriptorLayout;
@@ -138,6 +168,9 @@ public:
     uint32_t _graphicsQueueFamily;
 
     struct SDL_Window *_window{nullptr};
+
+    MaterialInstance defaultData;
+    GLTFMetallic_Roughness metalRoughMaterial;
 
     static VulkanEngine &Get();
 
